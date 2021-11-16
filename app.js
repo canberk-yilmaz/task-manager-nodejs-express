f8cf8d482647ead813d7ea949b972473cbeb37e5
canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-17 00:24:25 +0300

    feat: async db connection start function create

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config()
+// middleware
+
+app.use(express.json());
+
+//routes
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api/v1/tasks')         -get all the tasks
+// app.post('/api/v1/tasks')        -create a new task
+// app.get('/api/v1/tasks/:id')     -get single task
+// app.patch('/api/v1/tasks/:id')   -edit task
+// app.delete('/api/v1/tasks/:id')  -delete task
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+      console.error(error)
+  }
+};
+
+start()
